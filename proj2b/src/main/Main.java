baa98000464b08db083897ab4abd572ae2116342
MufanColin <76479709+MufanColin@users.noreply.github.com>
2024-06-29 22:21:29 +0800

    added proj2a into proj2b code, feel free to go back to the previous version

--- proj2b/src/main/Main.java
+++ proj2b/src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import browser.NgordnetServer;
+import ngrams.NGramMap;
 import org.slf4j.LoggerFactory;
 import wordnet.WordNet;
 
@@ -11,16 +12,21 @@ public class Main {
     public static void main(String[] args) {
         NgordnetServer hns = new NgordnetServer();
 
+        String wordFile = "./data/ngrams/top_14377_words.csv";
+        String countFile = "./data/ngrams/total_counts.csv";
+        NGramMap ngm = new NGramMap(wordFile, countFile);
+
         String synsetFile = "./data/wordnet/synsets.txt";
         String hyponymFile = "./data/wordnet/hyponyms.txt";
-
         WordNet wordNet = new WordNet(synsetFile, hyponymFile);
 
         hns.startUp();
-        hns.register("history", new DummyHistoryHandler());
-        hns.register("historytext", new DummyHistoryTextHandler());
+        hns.register("history", new HistoryHandler(ngm));
+        hns.register("historytext", new HistoryTextHandler(ngm));
         hns.register("hyponyms", new HyponymsHandler(wordNet));
 
+        // hns.register("history", new DummyHistoryHandler());
+        // hns.register("historytext", new DummyHistoryTextHandler());
         System.out.println("Finished server startup! Visit http://localhost:4567/ngordnet.html");
     }
 }
---
+++ proj2b/src/ngrams/TimeSeries.java
@@ -0,0 +1,102 @@
+package ngrams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+/**
+ * An object for mapping a year number (e.g. 1996) to numerical data. Provides
+ * utility methods useful for data analysis.
+ *
+ * @author Josh Hug
+ */
+public class TimeSeries extends TreeMap<Integer, Double> {
+
+    /** If it helps speed up your code, you can assume year arguments to your NGramMap
+     * are between 1400 and 2100. We've stored these values as the constants
+     * MIN_YEAR and MAX_YEAR here. */
+    public static final int MIN_YEAR = 1400;
+    public static final int MAX_YEAR = 2100;
+
+    /**
+     * Constructs a new empty TimeSeries.
+     */
+    public TimeSeries() {
+        super();
+    }
+
+    /**
+     * Creates a copy of TS, but only between STARTYEAR and ENDYEAR,
+     * inclusive of both end points.
+     */
+    public TimeSeries(TimeSeries ts, int startYear, int endYear) {
+        super();
+        for (Integer year: ts.years()) {
+            if (startYear <= year && year <= endYear) {
+                this.put(year, ts.get(year));
+            }
+        }
+    }
+
+    /**
+     * Returns all years for this TimeSeries (in any order).
+     */
+    public List<Integer> years() {
+        return new ArrayList<>(this.keySet());
+    }
+
+    /**
+     * Returns all data for this TimeSeries (in any order).
+     * Must be in the same order as years().
+     */
+    public List<Double> data() {
+        return new ArrayList<>(this.values());
+    }
+
+    /**
+     * Returns the year-wise sum of this TimeSeries with the given TS. In other words, for
+     * each year, sum the data from this TimeSeries with the data from TS. Should return a
+     * new TimeSeries (does not modify this TimeSeries).
+     *
+     * If both TimeSeries don't contain any years, return an empty TimeSeries.
+     * If one TimeSeries contains a year that the other one doesn't, the returned TimeSeries
+     * should store the value from the TimeSeries that contains that year.
+     */
+    public TimeSeries plus(TimeSeries ts) {
+        TimeSeries newTimeSeries = new TimeSeries();
+        for (Integer year: this.years()) {
+            if (ts.containsKey(year)) {
+                newTimeSeries.put(year, this.get(year) + ts.get(year));
+            } else {
+                newTimeSeries.put(year, this.get(year));
+            }
+        }
+        for (Integer year: ts.years()) {
+            if (!newTimeSeries.containsKey(year)) {
+                newTimeSeries.put(year, ts.get(year));
+            }
+        }
+        return newTimeSeries;
+    }
+
+    /**
+     * Returns the quotient of the value for each year this TimeSeries divided by the
+     * value for the same year in TS. Should return a new TimeSeries (does not modify this
+     * TimeSeries).
+     *
+     * If TS is missing a year that exists in this TimeSeries, throw an
+     * IllegalArgumentException.
+     * If TS has a year that is not in this TimeSeries, ignore it.
+     */
+    public TimeSeries dividedBy(TimeSeries ts) {
+        TimeSeries newTimeSeries = new TimeSeries();
+        for (Integer year: this.years()) {
+            if (!ts.containsKey(year)) {
+                throw new IllegalArgumentException();
+            } else {
+                newTimeSeries.put(year, this.get(year) / ts.get(year));
+            }
+        }
+        return newTimeSeries;
+    }
+}
